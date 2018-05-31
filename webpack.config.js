2ede923ad4896f8ee7476685219414336041ac67
Elizabeth Li <sincerelylibby@gmail.com>
2018-05-31 16:56:23 -0700

    Set up project dependencies.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, '/client/src/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react'],
+        },
+      },
+    ],
+  },
+};
