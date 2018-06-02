c812b0032c01a5d390b309f7491ac02489a8a8d1
Elizabeth Li <sincerelylibby@gmail.com>
2018-06-01 19:09:28 -0700

    Fix gitignore and bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: path.resolve(__dirname, './client/index.jsx'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './public'),
+    path: path.resolve(__dirname, './public/dist'),
   },
   module: {
     rules: [
