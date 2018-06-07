1579c8c2cc3ca88112c9d2448252bf8749ead0c3
Elizabeth Li <sincerelylibby@gmail.com>
2018-06-06 18:22:46 -0700

    Minor edit to text in section.

--- client/components/Shipping.jsx
+++ client/components/Shipping.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Shipping = ({shippingCountries, shippingPrice, shippingMin, shippingMax}) => {
+  const smallerText = {
+    fontSize: 14,
+  };
+
   return (
     <div>
       <h4>Shipping & returns</h4>
       <p>
         Made just for you. Ready to ship in {shippingMin}–{shippingMax} business days.<br />
         From United States<br />
-        $14.00 shipping to United States, 94109
+        $14.00 shipping to United States, 94109<br />
+        <span style={smallerText}>Shipping upgrades available in the cart</span>
       </p>
       <p>
         <strong>Returns and exchanges accepted</strong><br />
